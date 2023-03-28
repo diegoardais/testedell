327cf44070761e5476842bc5788e33c509ef1399
Diego Ardais <diego.ardais@edu.pucrs.br>
2023-03-28 14:09:11 -0300

    async await menu

--- index.js
+++ index.js
@@ -54,51 +54,46 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-function exibirMenu() {
+async function exibirMenu() {
   console.log('---------------------------');
   console.log('Selecione uma opção:');
   console.log('1. Consultar trechos x modalidade');
   console.log('2. Cadastrar transporte');
   console.log('3. Dados estatísticos');
   console.log('4. Finalizar o programa');
-  rl.question('Opção: ', option => {
-    switch (option) {
-      case '1':
-        questaoUm();
-        break;
-      case '2':
-        questaoDois();
-        break;
-      case '3':
-        questaoTres();
-        break;
-      case '4':
-        console.log('Saindo...');
-        rl.close();
-        process.exit();
-        break;
-      default:
-        console.log('Opção inválida.');
-        exibirMenu();
-        break;
-    }
-  });
+  const option = await rl.question('Opção: ');
+  switch (option) {
+    case '1':
+      await questaoUm();
+      break;
+    case '2':
+      await questaoDois();
+      break;
+    case '3':
+      await questaoTres();
+      break;
+    case '4':
+      console.log('Saindo...');
+      rl.close();
+      process.exit();
+      break;
+    default:
+      console.log('Opção inválida.');
+      await exibirMenu();
+      break;
+  }
 }
 
-function questaoUm() {
-  rl.question('Qual a cidade de origem? ', origem => {
-    cidadePartida = origem.toUpperCase();
-    rl.question('Qual a cidade de destino? ', name => {
-      cidadeDestino = name.toUpperCase();
-      rl.question(
-        'Qual o tipo de caminhao? [PEQUENO-MEDIO-GRANDE] ',
-        caminhao => {
-          tipoCaminhao = caminhao.toUpperCase();
-          consultarTrecho();
-        }
-      );
-    });
-  });
+async function questaoUm() {
+  const origem = await rl.question('Qual a cidade de origem? ');
+  const cidadePartida = origem.toUpperCase();
+  const name = await rl.question('Qual a cidade de destino? ');
+  const cidadeDestino = name.toUpperCase();
+  const caminhao = await rl.question(
+    'Qual o tipo de caminhao? [PEQUENO-MEDIO-GRANDE] '
+  );
+  const tipoCaminhao = caminhao.toUpperCase();
+  consultarTrecho();
 }
 
 async function consultarTrecho() {
@@ -397,7 +392,7 @@ async function cadastrarTransporte() {
         )} de ${cidadePartida} até ${cidadeDestino} com parada em ${cidadeParada}.`
     );
   }
-  exibirMenu();
+  await exibirMenu();
 }
 
 exibirMenu();
